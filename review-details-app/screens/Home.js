28b7d9ff1de12ed940f8cf0f2d7f179132446164
gomathiCoder <91526592+gomathiCoder@users.noreply.github.com>
2022-08-14 13:39:37 +0530

    feat: Navigating around

--- review-details-app/screens/Home.js
+++ review-details-app/screens/Home.js
@@ -1,11 +1,15 @@
-import { Text, View, StyleSheet} from 'react-native';
+import { Text, View, Button} from 'react-native';
 import React from 'react';
 import { globalStyles  } from '../styles/global';
 
-export default function Home(){
+export default function Home({ navigation }){
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>Home screen</Text>
+            <Button 
+                title='Go to Review Details'
+                onPress={() => navigation.navigate('ReviewDetails')}
+            />
         </View>
     )
 }
--- review-details-app/screens/ReviewDetails.js
+++ review-details-app/screens/ReviewDetails.js
@@ -1,11 +1,15 @@
-import { Text, View, StyleSheet} from 'react-native';
+import { Text, View, Button} from 'react-native';
 import React from 'react';
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetails(){
+export default function ReviewDetails({ navigation }){
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>Review Details screen</Text>
+            <Button 
+                title='Go to Home'
+                onPress={() => navigation.navigate('Home')}
+            />
         </View>
     )
 }
