20caf17388812492b1c59798f792bae1c6b4c35b
gomathiCoder <91526592+gomathiCoder@users.noreply.github.com>
2022-08-14 19:41:37 +0530

    style: navigation option

--- review-details-app/routes/HomeStack.js
+++ review-details-app/routes/HomeStack.js
@@ -5,13 +5,25 @@ import ReviewDetails from '../screens/ReviewDetails';
 
 const screens = {
     Home : {
-        screen : Home
+        screen : Home,
+        navigationOptions: {
+            title: 'Game Zone'
+        }
     },
     ReviewDetails : {
-        screen : ReviewDetails
+        screen : ReviewDetails,
+        navigationOptions: {
+            title: 'Review Details'
+        }
     }
 }
 
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens,
+    {
+        defaultNavigationOptions:{
+            headerTintColor: '#444',
+            headerStyle: { backgroundColor: '#ddd', height: 80}
+        }
+    });
 
 export default createAppContainer(HomeStack);
