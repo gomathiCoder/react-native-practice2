747d27baaf51983213f7c509a9f883c973f97777
gomathiCoder <91526592+gomathiCoder@users.noreply.github.com>
2022-09-10 19:06:55 +0530

    style: displayed ratings using images

--- review-details-app/screens/ReviewDetails.js
+++ review-details-app/screens/ReviewDetails.js
@@ -1,7 +1,8 @@
-import { Text, View } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
 import React, {useEffect} from 'react';
-import { globalStyles } from '../styles/global';
+import { globalStyles, images } from '../styles/global';
 import Card from '../shared/card';
+
 
 export default function ReviewDetails({ route, navigation }){
 
@@ -9,15 +10,31 @@ export default function ReviewDetails({ route, navigation }){
         navigation.getParent().setOptions({headerShown: false});
 
         return () => navigation.getParent().setOptions({headerShown:true});
-    }, [navigation])
+    }, [navigation]);
+
+    const rating = route.params.rating;
 
     return (
         <View style={globalStyles.container}>
             <Card>
                 <Text>{ route.params.title }</Text>
                 <Text>{ route.params.body }</Text>
-                <Text>{ route.params.rating }</Text>
+                <View style={styles.rating}>
+                <Text>GameZone Rating : </Text>
+                <Image source={images.ratings[rating]} />
+                </View>
             </Card>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    rating:{
+        flexDirection :'row',
+        justifyContent: 'center',
+        paddingTop: 16,
+        marginTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: '#eee'
+    }
+})
