7ab88f81e33db4a0f492cdbbcbb3e81575202b80
gomathiCoder <91526592+gomathiCoder@users.noreply.github.com>
2022-09-14 12:22:40 +0530

    feat: Modal view added

--- review-details-app/screens/Home.js
+++ review-details-app/screens/Home.js
@@ -1,9 +1,11 @@
-import { Text, View, Button, FlatList, TouchableOpacity } from 'react-native';
+import { Text, View, Button, FlatList, TouchableOpacity, Modal, StyleSheet } from 'react-native';
 import React, { useState } from 'react';
 import { globalStyles  } from '../styles/global';
+import { MaterialIcons } from '@expo/vector-icons';
 import Card from '../shared/card';
 
 export default function Home({ navigation }){
+    const [modalOpen, setModalOpen] = useState(false);
     const [reviews, setReviews] = useState([
         {title: 'Zelda, Breath of Fresh Air', rating:5, body:'lorem ipsum', key:'1'},
         {title: 'Gotta Catch Them All (again)', rating:4, body:'lorem-ipsum', key:'2'},
@@ -12,6 +14,24 @@ export default function Home({ navigation }){
 
     return (
         <View style={globalStyles.container}>
+            <Modal visible={modalOpen} animationType='slide'>
+                <View style={styles.modalContent}>
+                    <MaterialIcons
+                        name='close'
+                        size={24}
+                        onPress={() => setModalOpen(false)}
+                        style={{...styles.modalToggle, ...styles.modalClose}}
+                    />
+                </View>
+            </Modal>
+
+            <MaterialIcons
+                name='add'
+                size={24}
+                onPress={() => setModalOpen(true)}
+                style={styles.modalToggle}
+            />
+
             <FlatList
                 data={reviews}
                 renderItem={({item}) => (
@@ -25,3 +45,23 @@ export default function Home({ navigation }){
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    modalToggle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: '#f2f2f2',
+        padding: 10,
+        borderRadius: 10,
+        alignSelf: 'center'
+    },
+    modalClose: {
+        marginTop: 20,
+        marginBottom: 0
+    },
+    modalContent:{
+        flex: 1
+    }
+});
